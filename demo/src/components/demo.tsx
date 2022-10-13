f320a56cdd52810e5efc497c3700251724b0ab29
Eric <37158449+eluce2@users.noreply.github.com>
2022-10-13 10:39:53 -0500

    fix build

---
+++ demo/src/components/demo.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { fmFetch } from "@proofgeist/fm-webviewer-fetch";
+
+export default function Demo() {
+  const [fmData, setFmData] = React.useState<any>();
+
+  async function getDataFromFM() {
+    const data = await fmFetch("GetSimpleResult", {
+      param1: "Hello",
+      param2: "World",
+    });
+    setFmData(data);
+  }
+  return (
+    <div style={{ padding: "20px", display: "grid", gap: "20px" }}>
+      <div>
+        <h1>fmFetch Example</h1>
+        <p>
+          Click this button to run a FileMaker script and get the result back
+          into this webpage.
+        </p>
+        <button onClick={() => getDataFromFM()} type="button">
+          Fetch Data
+        </button>
+      </div>
+
+      <div>
+        <h3>Name Field Fetched from FileMaker</h3>
+        <div>{fmData?.name}</div>
+      </div>
+
+      <div>
+        <h3>Layout Names Fetched From FileMaker</h3>
+        <ul id="layouts">
+          {fmData?.layoutNames?.map((layout: any) => (
+            <li>{layout}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
--- demo/src/pages/demo.tsx
+++ demo/src/pages/demo.tsx
@@ -1,48 +1,55 @@
 import React from "react";
-import { fmFetch } from "@proofgeist/fm-webviewer-fetch";
 import BrowserOnly from "@docusaurus/BrowserOnly";
+import FetchLibrary from "@proofgeist/fm-webviewer-fetch";
 
 export default function Demo() {
   const [fmData, setFmData] = React.useState<any>();
 
-  async function getDataFromFM() {
-    const data = await fmFetch("GetSimpleResult", {
-      param1: "Hello",
-      param2: "World",
-    });
-    setFmData(data);
-  }
-
   return (
-    <BrowserOnly>
-      {() => (
-        <div style={{ padding: "20px", display: "grid", gap: "20px" }}>
-          <div>
-            <h1>fmFetch Example</h1>
-            <p>
-              Click this button to run a FileMaker script and get the result
-              back into this webpage.
-            </p>
-            <button onClick={() => getDataFromFM()} type="button">
-              Fetch Data
-            </button>
-          </div>
+    <BrowserOnly fallback={<div>Loading...</div>}>
+      {() => {
+        /** NOTE: this method of importing the module is only required for the docs website  */
+        const { fmFetch } =
+          require("@proofgeist/fm-webviewer-fetch") as typeof FetchLibrary;
 
-          <div>
-            <h3>Name Field Fetched from FileMaker</h3>
-            <div>{fmData?.name}</div>
-          </div>
+        async function getDataFromFM() {
+          const data = await fmFetch("GetSimpleResult", {
+            param1: "Hello",
+            param2: "World",
+          });
+          setFmData(data);
+        }
 
-          <div>
-            <h3>Layout Names Fetched From FileMaker</h3>
-            <ul id="layouts">
-              {fmData?.layoutNames?.map((layout: any) => (
-                <li>{layout}</li>
-              ))}
-            </ul>
+        const Page = (
+          <div style={{ padding: "20px", display: "grid", gap: "20px" }}>
+            <div>
+              <h1>fmFetch Example</h1>
+              <p>
+                Click this button to run a FileMaker script and get the result
+                back into this webpage.
+              </p>
+              <button onClick={getDataFromFM} type="button">
+                Fetch Data
+              </button>
+            </div>
+
+            <div>
+              <h3>Name Field Fetched from FileMaker</h3>
+              <div>{fmData?.name}</div>
+            </div>
+
+            <div>
+              <h3>Layout Names Fetched From FileMaker</h3>
+              <ul id="layouts">
+                {fmData?.layoutNames?.map((layout: any) => (
+                  <li>{layout}</li>
+                ))}
+              </ul>
+            </div>
           </div>
-        </div>
-      )}
+        );
+        return Page;
+      }}
     </BrowserOnly>
   );
 }
