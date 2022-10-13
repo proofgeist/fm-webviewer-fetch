1cfee1f38e7080ff5f5f938384b797e67b601a9b
Eric <37158449+eluce2@users.noreply.github.com>
2022-10-13 10:12:21 -0500

    update demo page

--- demo/src/pages/demo.tsx
+++ demo/src/pages/demo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { fmFetch } from "@proofgeist/fm-webviewer-fetch";
+import BrowserOnly from "@docusaurus/BrowserOnly";
 
 export default function Demo() {
   const [fmData, setFmData] = React.useState<any>();
@@ -13,31 +14,35 @@ export default function Demo() {
   }
 
   return (
-    <div style={{ padding: "20px", display: "grid", gap: "20px" }}>
-      <div>
-        <h1>fmFetch Example</h1>
-        <p>
-          Click this button to run a FileMaker script and get the result back
-          into this webpage.
-        </p>
-        <button onClick={() => getDataFromFM()} type="button">
-          Fetch Data
-        </button>
-      </div>
+    <BrowserOnly>
+      {() => (
+        <div style={{ padding: "20px", display: "grid", gap: "20px" }}>
+          <div>
+            <h1>fmFetch Example</h1>
+            <p>
+              Click this button to run a FileMaker script and get the result
+              back into this webpage.
+            </p>
+            <button onClick={() => getDataFromFM()} type="button">
+              Fetch Data
+            </button>
+          </div>
 
-      <div>
-        <h3>Name Field Fetched from FileMaker</h3>
-        <div>{fmData?.name}</div>
-      </div>
+          <div>
+            <h3>Name Field Fetched from FileMaker</h3>
+            <div>{fmData?.name}</div>
+          </div>
 
-      <div>
-        <h3>Layout Names Fetched From FileMaker</h3>
-        <ul id="layouts">
-          {fmData?.layoutNames?.map((layout: any) => (
-            <li>{layout}</li>
-          ))}
-        </ul>
-      </div>
-    </div>
+          <div>
+            <h3>Layout Names Fetched From FileMaker</h3>
+            <ul id="layouts">
+              {fmData?.layoutNames?.map((layout: any) => (
+                <li>{layout}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </BrowserOnly>
   );
 }
