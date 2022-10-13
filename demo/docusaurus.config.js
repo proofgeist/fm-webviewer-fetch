c99dca983475e8526471a9a2ca697ee5dd6f3523
Eric <37158449+eluce2@users.noreply.github.com>
2022-10-13 08:22:54 -0500

    change edit url

--- demo/docusaurus.config.js
+++ demo/docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://pr.new/github.com/proofgeist/fm-webviewer-fetch/tree/main",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
